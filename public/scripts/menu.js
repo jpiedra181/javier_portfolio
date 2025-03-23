b6bf7c3a8bb64f1052497d1a94bd54d156b77a74
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-23 11:53:13 +0100

    updating menu.js

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -3,26 +3,26 @@ const menuIconClose = document.querySelector('.menuIconClose')
 const menuItems = document.querySelector('.menuItems')
 const items = document.querySelectorAll('.item')
 
-menuIconOpen.addEventListener('click', () => {
-    menuIconOpen.style.display = "none"
-    menuIconClose.style.display = "block"
-    menuItems.style.display = "block"
-    items.forEach(item => {
-        item.addEventListener('click', () => {
-            menuIconOpen.style.display = "block"
-            menuIconClose.style.display = "none"
-            menuItems.style.display = "none"
-        })
-    })
-})
-
-menuIconClose.addEventListener('click', () => {
-    menuIconOpen.style.display = "block"
-    menuIconClose.style.display = "none"
-    menuItems.style.display = "none"
-})
-
 if(window.innerWidth >= 768) {
     menuIconOpen.style.display = "none"
     menuIconClose.style.display = "none"
+} else {
+    menuIconOpen.addEventListener('click', () => {
+        menuIconOpen.style.display = "none"
+        menuIconClose.style.display = "block"
+        menuItems.style.display = "block"
+        items.forEach(item => {
+            item.addEventListener('click', () => {
+                menuIconOpen.style.display = "block"
+                menuIconClose.style.display = "none"
+                menuItems.style.display = "none"
+            })
+        })
+    })
+
+    menuIconClose.addEventListener('click', () => {
+        menuIconOpen.style.display = "block"
+        menuIconClose.style.display = "none"
+        menuItems.style.display = "none"
+    })
 }
