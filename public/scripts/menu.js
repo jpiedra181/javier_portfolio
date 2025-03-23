cced9bba781c71e7699a73988411d73ddd60156c
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-23 11:58:09 +0100

    updating menu.js 3

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -3,29 +3,27 @@ const menuIconClose = document.querySelector('.menuIconClose')
 const menuItems = document.querySelector('.menuItems')
 const items = document.querySelectorAll('.item')
 
-if(window.innerWidth >= 768) {
+
+menuIconOpen.addEventListener('click', () => {
     menuIconOpen.style.display = "none"
-    menuIconClose.style.display = "none"
-} else {
-    menuIconOpen.style.display = "block"
-    menuIconClose.style.display = "none"
-    
-    menuIconOpen.addEventListener('click', () => {
-        menuIconOpen.style.display = "none"
-        menuIconClose.style.display = "block"
-        menuItems.style.display = "block"
-        items.forEach(item => {
-            item.addEventListener('click', () => {
-                menuIconOpen.style.display = "block"
-                menuIconClose.style.display = "none"
-                menuItems.style.display = "none"
-            })
+    menuIconClose.style.display = "block"
+    menuItems.style.display = "block"
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            menuIconOpen.style.display = "block"
+            menuIconClose.style.display = "none"
+            menuItems.style.display = "none"
         })
     })
+})
 
-    menuIconClose.addEventListener('click', () => {
-        menuIconOpen.style.display = "block"
+menuIconClose.addEventListener('click', () => {
+    menuIconOpen.style.display = "block"
+    menuIconClose.style.display = "none"
+    menuItems.style.display = "none"
+
+    if(window.innerWidth >= 768) {
+        menuIconOpen.style.display = "none"
         menuIconClose.style.display = "none"
-        menuItems.style.display = "none"
-    })
-}
+    }
+})
