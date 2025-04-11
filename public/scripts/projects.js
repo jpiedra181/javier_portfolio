dd6261a72e5ee0ff3d0ba47ad1b8d9a5c3bc7459
JPiedra <javierpiedraiglesias@gmail.com>
2025-04-11 23:23:30 +0200

    modal functionality

--- public/scripts/projects.js
+++ public/scripts/projects.js
@@ -1,4 +1,12 @@
 const projects = document.querySelectorAll(".project")
+const modal =document.querySelector('.modal')
+const info = document.querySelector('projectInfo')
+const close = document.querySelector('.close')
+const image = document.querySelector('.projectImage')
+const title = document.querySelector('.title')
+const description = document.querySelector('.description')
+const link = document.querySelector('.link')
+const techs = document.querySelector('.tech')
 
 async function getProject() {
     try{
@@ -6,7 +14,21 @@ async function getProject() {
         const data = await response.json()
         console.log(data)
         projects.forEach((project) => {
+            project.addEventListener('click', () => {
+                if(project.classList.contains('ecommerce')) {let show = data.projects[0]}
+                if(project.classList.contains('todo')) {let show = data.projects[1]}
+                if(project.classList.contains('discord')) {let show = data.projects[2]}
+                if(project.classList.contains('foodie')) {let show = data.projects[3]}
+                if(project.classList.contains('blog')) {let show = data.projects[4]}
+                if(project.classList.contains('game')) {let show = data.projects[5]}
+                modal.style.display = 'flex'
+                modal.classList.add('w-screen h-screen blur-3xl justify-center items-center')
+                title.innerHTML = show.title
+                description.innerHTML = show.description
+                image.src = show.image
+                link.href = show.link
 
+            })
         })
     } catch{
 
