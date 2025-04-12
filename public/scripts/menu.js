c6b1419b45d7ee53a953ac3d9ac235650f0c5a9f
JPiedra <javierpiedraiglesias@gmail.com>
2025-04-12 18:35:28 +0200

    add lang button

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -2,8 +2,9 @@ const menuIconOpen = document.querySelector('.menuIconOpen')
 const menuIconClose = document.querySelector('.menuIconClose')
 const menuItems = document.querySelector('.menuItems')
 const items = document.querySelectorAll('.item')
+const langBtn = document.getElementById('langBtn')
 
-
+// Menu Icon
 menuIconOpen.addEventListener('click', () => {
     menuIconOpen.style.display = "none"
     menuIconClose.style.display = "block"
@@ -22,3 +23,22 @@ menuIconClose.addEventListener('click', () => {
     menuIconClose.style.display = "none"
     menuItems.style.display = "none"
 })
+
+// Change the language
+let currentLang = localStorage.getItem('lang') || 'en'
+
+function updateLanguage(lang) {
+    document.querySelectorAll('[data-en]').forEach(el => {
+      el.textContent = el.dataset[lang]
+    });
+    langBtn.textContent = lang === 'en' ? 'ES' : 'EN'
+    localStorage.setItem('lang', lang)
+  }
+  
+  langBtn.addEventListener('click', () => {
+    currentLang = currentLang === 'en' ? 'es' : 'en'
+    updateLanguage(currentLang)
+  });
+  
+  // Init
+  updateLanguage(currentLang);
